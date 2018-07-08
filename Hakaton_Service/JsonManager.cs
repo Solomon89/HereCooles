b162bbdd6ef7a4306bdccc2c196b3c91bddb31f6
Gorser <sergik-gorshkov@mail.ru>
2018-07-08 10:34:07 +0300

    JsonUpdate

--- Hakaton_Service/JsonManager.cs
+++ Hakaton_Service/JsonManager.cs
@@ -1,6 +1,5 @@
-﻿using System.IO;
+﻿using Newtonsoft.Json;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 
 namespace Hakaton_Service
 {
@@ -8,21 +7,12 @@ namespace Hakaton_Service
     {
         public static string GetJsonString<T>(T obj)
         {
-            var jsonSerializer = new DataContractJsonSerializer(typeof(T));
-            var stream = new MemoryStream();
-            jsonSerializer.WriteObject(stream, obj);
-            stream.Seek(0, SeekOrigin.Begin);
-            return new StreamReader(stream).ReadToEnd();
+            return JsonConvert.SerializeObject(obj);
         }
 
         public static T FromJson<T>(string obj)where T : new()
         {
-            var jsonSerializer = new DataContractJsonSerializer(typeof(T));
-            var stream = new MemoryStream();
-            new StreamWriter(stream).Write(obj);
-            stream.Seek(0, SeekOrigin.Begin);
-            //return (T) jsonSerializer.ReadObject(stream);
-            return new T();
+            return JsonConvert.DeserializeObject<T>(obj);
         }
 
         public static string JsonError(string message) => GetJsonString(new ErrorClass {Message = message});
